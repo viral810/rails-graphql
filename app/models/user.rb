579c904cc84bf2d8171c0558040f4166d916983d
Viral Patel <viralpatel@Virals-MacBook-Pro-2.local>
2017-11-06 13:16:14 -0500

    added api_token to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_secure_token :api_token
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  email              :string           not null
+#  encrypted_password :string(128)      not null
+#  confirmation_token :string(128)
+#  remember_token     :string(128)      not null
+#  api_token          :string
